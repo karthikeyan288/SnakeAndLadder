2c2c1d4fa9ad209b3fd2aaa386af7b0a96306f06
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-03 08:20:25 +0530

    ADDING: snake and ladder uc2

---
+++ src/com/SnakeLadder/ext/SnakeLadderUc2.java
@@ -0,0 +1,12 @@
+package com.SnakeLadder.ext;
+
+public class SnakeLadderUc2 {
+	public static void main(String[] args) {
+		int player=0;
+		int position =0;
+		double die_value = Math.floor(Math.random()* 10) %6 +1;
+        //die value is between 0 to 6
+	    System.out.println(die_value);
+	
+	}
+}
